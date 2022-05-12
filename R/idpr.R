7278835b2626a1edc9d72df7736e7a04c64b0a17
wmm27 <wmm27@pitt.edu>
2022-05-11 22:20:44 -0400

    idpr documentation

---
+++ R/idpr.R
@@ -0,0 +1,15 @@
+#' idpr: profiling and analyzing Intrinsically Disordered Proteins in R
+#'
+#' idpr aims to integrate tools for the computational analysis of 
+#' intrinsically disordered proteins (IDPs) within R. This package is used to 
+#' identify known characteristics of IDPs for a sequence of interest with 
+#' easily reported and dynamic results. Additionally, this package includes 
+#' tools for IDP-based sequence analysis to be used in conjunction with other R 
+#' packages. 
+#' \cr
+#' Please see the idpr vignettes for details on idpr functions and theory.
+#' \code{browseVignettes("idpr")}
+#' @docType package
+#' @name idpr
+NULL
+#> NULL
